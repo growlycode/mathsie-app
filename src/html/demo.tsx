6338b0918a0f8d85a46358df51c85e23aa52ce20
Dana Chapman <dm.chapman@gmail.com>
2024-08-12 22:45:02 +1200

    Detect pointer type

--- src/html/demo.tsx
+++ src/html/demo.tsx
@@ -1,32 +1,33 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function HtmlDemo() {
     const ref = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-        const ctx: CanvasRenderingContext2D | null | undefined  = ref?.current?.getContext('2d');
-        console.log(ctx?.canvas.width);
+    const [isPen, setIsPen] = useState<boolean>(true);
+    const [pointerType, setPointerType] = useState<string>('?');
 
+    useEffect(() => {
+        const ctx: CanvasRenderingContext2D | null | undefined = ref?.current?.getContext('2d');
         const canvas = ctx?.canvas;
         if (!ctx || !canvas) return;
 
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
-    
+
         ctx.strokeStyle = '#51c9bb';
         ctx.lineJoin = 'round';
         ctx.lineCap = 'round';
         ctx.lineWidth = 30;
-    
+
         //flag
         let isDrawing = false; //don't draw when mouse is just moving mouse w/o doing anything
-    
+
         //where to start a line from and then where to end it
         let lastX = 0;
         let lastY = 0;
         let hue = 0;
         let direction = true;
-    
+
         function draw(e: any) {
             if (!isDrawing || !ctx)
                 return; //only run in click and drag
@@ -39,7 +40,7 @@ export function HtmlDemo() {
             [lastX, lastY] = [e.offsetX, e.offsetY];
             // lastX=e.offsetX;
             // lastY=e.offsetY;
-    
+
             hue++;
             if (hue >= 360) {
                 hue = 0;
@@ -52,50 +53,51 @@ export function HtmlDemo() {
             else
                 ctx.lineWidth--;
         }
-    
+
+        canvas.addEventListener('pointerdown',
+            (event: any) => {
+                setIsPen(event.pointerType === 'pen');
+                setPointerType(event.pointerType);
+            },
+            false
+        );
         canvas.addEventListener('mousemove', draw);
         canvas.addEventListener('mousedown', (e) => {
             isDrawing = true;
             [lastX, lastY] = [e.offsetX, e.offsetY];
         });
-    
-    
+
+
         canvas.addEventListener('mouseup', () => isDrawing = false);
         canvas.addEventListener('mouseout', () => isDrawing = false);
 
-        
-    //canvas on mobile
 
-    document.body.addEventListener("touchstart", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-            const clientX = e.touches[0].clientX;
-            const clientY = e.touches[0].clientY;
-            isDrawing = true;
-            draw({ offsetX: clientX, offsetY: clientY })
-        }
-    }, false);
-    document.body.addEventListener("touchend", function (e) {
-        if (e.target == canvas) {
-            e.preventDefault();
-            isDrawing = false;
-        }
-    }, false);
-    document.body.addEventListener("touchmove", function (e: any) {
-        if (e.target == canvas) {
-            e.preventDefault();
-            e.offsetX = e.targetTouches[0].clientX;
-            e.offsetY = e.targetTouches[0].clientY;
-            draw(e)
-        }
-    }, false);
+        //canvas on mobile
+
+        document.body.addEventListener("touchstart", function (e) {
+            if (e.target == canvas) {
+                e.preventDefault();
+                const clientX = e.touches[0].clientX;
+                const clientY = e.touches[0].clientY;
+                isDrawing = true;
+                draw({ offsetX: clientX, offsetY: clientY })
+            }
+        }, false);
+        document.body.addEventListener("touchend", function (e) {
+            if (e.target == canvas) {
+                e.preventDefault();
+                isDrawing = false;
+            }
+        }, false);
+        document.body.addEventListener("touchmove", function (e: any) {
+            if (e.target == canvas) {
+                e.preventDefault();
+                e.offsetX = e.targetTouches[0].clientX;
+                e.offsetY = e.targetTouches[0].clientY;
+                draw(e)
+            }
+        }, false);
     }, [])
 
-
-
-
-
-    
-
-    return <canvas id="draw" width="800" height="800"  ref={ref}></canvas>;
+    return <><div>Pointer type: {pointerType}</div><canvas id="draw" width="800" height="800" ref={ref}></canvas></>;
 }
