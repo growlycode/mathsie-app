ad6c9247dfe7b741f357367ea2612a799505ddce
Dana Chapman <dm.chapman@gmail.com>
2024-08-14 15:55:15 +1200

    Use window.screen.height instead of innerheight

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ function WorksheetPage({ uworksheet, isMarking, onSave }: {
     const div = ref.current;
     if (!div) return;
 
-    const padding = window.innerHeight * 0.04;
+    const padding = window.screen.height * 0.04;
     const height = (div.getBoundingClientRect().height - (padding*2)) / uworksheet.worksheet.operations.length;
     div.style.fontSize = `calc(${height}px - 4vh)`;
 
