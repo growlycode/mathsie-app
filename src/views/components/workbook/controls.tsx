31c1841c21d4742c5d4390dd6931bfa86e912c26
Dana Chapman <dm.chapman@gmail.com>
2024-08-16 12:53:00 +1200

    Nicer styling

--- src/views/components/workbook/controls.tsx
+++ src/views/components/workbook/controls.tsx
@@ -1,5 +1,6 @@
 import LogoutButton from "../auth/logout-button";
 import { IconButton } from "../buttons/icon-button";
+import logo from "../../../assets/mathsie.svg";
 
 export function PageControls({ currentPage, totalPages, setPage }: {
   currentPage: number, totalPages: number, setPage: (page: number) => void
@@ -13,7 +14,8 @@ export function PageControls({ currentPage, totalPages, setPage }: {
     setPage(Math.max(0, currentPage - 1));
   }
 
-  return <div className='flex grow justify-between p-2'>
+  return <div className='flex grow justify-between p-2 gap-2 items-center border border-b'>
+    <img src={logo} className="logo h-[40px] mr-4" alt="mathsie logo" />
     <div className="flex grow gap-4 items-center">
       <IconButton onClick={prevSheet} faClass="chevron-left" />
       <span>Page {currentPage + 1} of {totalPages}</span>
