840b8dab366781638659582f822f71d026345ccf
Dana Chapman <dm.chapman@gmail.com>
2024-08-16 13:30:17 +1200

    Remove unused var

--- src/views/components/auth/logout-button.tsx
+++ src/views/components/auth/logout-button.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { IconButton } from "../buttons/icon-button";
 
 const LogoutButton = () => {
-  const { logout, isAuthenticated, user } = useAuth0();
+  const { logout, isAuthenticated } = useAuth0();
   
   return (isAuthenticated && <IconButton faClass="arrow-right-from-bracket fa-solid" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} />
   );
