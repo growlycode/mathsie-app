3d374e0fc41c569080e82a1b883388ae44160b66
Dana Chapman <dm.chapman@gmail.com>
2024-08-18 12:02:59 +1200

    Add in missing auth stuff

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLoading from './views/components/site/loading';
 import { PrivateRoute } from './views/routing/PrivateRoute';
 import { Login } from './views/components/auth/login';
+import { Env } from './infrastructure/env/env';
+import { useAuth } from './auth/hooks';
 ``
 
 function App() {
- // const { loading } = useAuth(auth);
+  const { loading } = useAuth(auth);
 
   return (<>
     <BrowserRouter>
-      {true
+      {loading
         ? <AppLoading />
         : <Routes>
           <Route path="/login" element={<Login />} />
@@ -23,7 +25,7 @@ function App() {
             ? <Route path="/*" element={<WorkbookPage />} />
             : <Route
               path='/*'
-              element={<PrivateRoute><WorkbookPage/></PrivateRoute>}
+              element={<PrivateRoute><WorkbookPage /></PrivateRoute>}
             />
           }
         </Routes>}
